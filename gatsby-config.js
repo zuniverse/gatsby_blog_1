4c17e80f4f8eeac7b9a75b4f37b49935dbb1d88c
Paul <paulzuliani@gmail.com>
2020-10-16 13:04:07 +0200

    added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,5 +23,29 @@ module.exports = {
     },
     `gatsby-plugin-sharp`, 
     `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`,
+        ],
+        display: 'swap',
+      },
+    },
   ],
 }
